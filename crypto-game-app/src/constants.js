cbabd9baa44e3c0c0397dd9f147c04981e4f4211
CZTopp <czshows@gmail.com>
2022-01-29 01:08:39 -0800

    add character select functions

---
+++ crypto-game-app/src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = "0xA40CD7dD2C825EF0ff1C11938df62F3ed79B676b"
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  }
+}
+
+export { CONTRACT_ADDRESS, transformCharacterData }
